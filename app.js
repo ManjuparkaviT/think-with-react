49203f8ffe1d4cf592f1a383019292e392120b2d
MANJUPARKAVI T <manjutv1999@gmail.com>
2021-02-16 08:42:43 +0530

    Updated

---
+++ app.js
@@ -0,0 +1,8 @@
+const react = document.getElementById("container")
+
+function Hello(){
+    return(
+    <div><p>Hey ProGrads!Welcome to React Learning<br></br>Lets rock and roll</p></div>
+    )
+}
+ReactDOM.render(<Hello/>,react)
